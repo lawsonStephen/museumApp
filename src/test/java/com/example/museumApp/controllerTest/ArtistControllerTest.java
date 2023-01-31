c84ffc567407710595813a7aa11401f9d24c9f1e
Isaac Gifford <isaacg@goellis.com>
2023-01-31 15:28:21 +0000

    basic controller tests for museum and artist working while advanced security is disabled

--- src/test/java/com/example/museumApp/controllerTest/ArtistControllerTest.java
+++ src/test/java/com/example/museumApp/controllerTest/ArtistControllerTest.java
@@ -1,4 +1,56 @@
 package com.example.museumApp.controllerTest;
 
+import com.example.museumApp.controller.ArtistController;
+import com.example.museumApp.model.Artist;
+import com.example.museumApp.service.ArtistService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+@WebMvcTest(ArtistController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ArtistControllerTest {
+
+    @MockBean
+    private ArtistService artistService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ArtistController uut;
+
+    @Test
+    public void doesUutExist(){
+        assertThat(uut != null);
+    }
+
+    @Test
+    public void doesGetAllArtistsReturnCorrectly() throws Exception {
+        ModelAndView mnV = new ModelAndView("artists");
+        Artist artist = new Artist();
+        List<Artist> artists = new ArrayList<>(Arrays.asList(artist));
+        artists.add(artist);
+        mnV.addObject("artists", artists);
+
+        when(artistService.findAll()).thenReturn(artists);
+        mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/artist"));
+        verify(artistService, times(1)).findAll();
+        assertThat(mnV.equals(uut.get()));
+    }
 }
