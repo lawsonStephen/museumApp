622a297b81128d326883e4435aeecb05c6a94395
Lawson <lawsonmoore98@hotmail.com>
2023-02-02 16:24:06 +0000

    Quick fix to index, added id to buttons for testing purposes

--- src/test/java/com/example/museumApp/integrationTest/test_runners/NavigationTestRunner.java
+++ src/test/java/com/example/museumApp/integrationTest/test_runners/NavigationTestRunner.java
@@ -1,4 +1,4 @@
-package com.example.museumApp.integrationTest;
+package com.example.museumApp.integrationTest.test_runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features="src/test/java/com/example/museumApp/integrationTest/features/navigation",
-        glue = "com/example/museumApp/integrationTest/step_definitions/navigation")
+        glue = "com/example/museumApp/integrationTest/step_definitions/navigation",
+        tags = "@UnderTest")
 public class NavigationTestRunner {
 
 }
